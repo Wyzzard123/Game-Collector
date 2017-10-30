5967530a5ecb840ec6785db12077ae0fbd05db60
Wyzzard123 <32074198+Wyzzard123@users.noreply.github.com>
2017-10-30 22:25:20 +0800

    Before adding dictionaries

---
+++ Book Collector/GameViewController.swift
@@ -0,0 +1,34 @@
+//
+//  GameViewController.swift
+//  Book Collector
+//
+//  Created by Wyz on 30/10/17.
+//  Copyright © 2017 Wyz. All rights reserved.
+//
+
+import UIKit
+
+class GameViewController: UIViewController {
+    @IBAction func photosTapped(_ sender: Any) {
+    }
+    
+    
+    @IBAction func cameraTapped(_ sender: Any) {
+    }
+    
+    @IBOutlet weak var titleTextField: UITextField!
+    
+    @IBOutlet weak var screenshotImageView: UIImageView!
+    
+    @IBAction func addTapped(_ sender: Any) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+  
+
+}
